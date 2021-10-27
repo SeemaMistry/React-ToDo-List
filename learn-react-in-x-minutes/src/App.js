4fa1f4ef840885e5f885ccae2acf346ca5b86c27
SeemaMistry <seema.m.1996@gmail.com>
2021-10-27 16:32:53 -0400

    Add all the html components to App.js for the app, buttons, input, a div for counting items. Remove hello World.

--- learn-react-in-x-minutes/src/App.js
+++ learn-react-in-x-minutes/src/App.js
@@ -1,9 +1,19 @@
 import TodoList from "./TodoList";
 
 function App() {
+  // create some html for the todo list using JSX
+
+  // Return can only return 1 thing, so 2 html elements cannot be returned.
+  // Fix: wrap all html-els into one empty element, called a "fragment" 
+  // <> [html code] </>
   return (
-    // create some html for the todo list using JSX
-    <TodoList />
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left todo</div>
+    </>
   )
 }
 
