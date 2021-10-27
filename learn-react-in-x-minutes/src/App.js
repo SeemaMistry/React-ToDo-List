d3d7e6ddc1b959f5458c283c12a012a0d5a4c5d0
SeemaMistry <seema.m.1996@gmail.com>
2021-10-27 17:31:41 -0400

    Import useState from react. Use the useState() function to create the default state of our app when its loads, an empty array. Destruct the array returned from useState() into todos and setTodos. Test todos by adding it as a parameter in TodoList() and printing out todos.length

--- learn-react-in-x-minutes/src/App.js
+++ learn-react-in-x-minutes/src/App.js
@@ -1,6 +1,17 @@
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
+  // 
+  /* create a state using useState hook imported from react 
+    - default is an empty array when app loads
+    - function returns an array
+    - destruct object into todos and setTodos variables
+    todos -> every single todos inside out todo-state
+    setTodos -> function called to update each of the todos
+  */
+  const [ todos, setTodos ] = useState(['Todo 1', 'Todo 2'])
+
   // create some html for the todo list using JSX
 
   // Return can only return 1 thing, so 2 html elements cannot be returned.
@@ -8,7 +19,7 @@ function App() {
   // <> [html code] </>
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos} />
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Completed Todos</button>
