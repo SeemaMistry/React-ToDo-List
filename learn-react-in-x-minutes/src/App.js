12aa7a33949e4d6a1f9c605a0fdf5e9b4b2b44b5
SeemaMistry <seema.m.1996@gmail.com>
2021-10-27 16:26:55 -0400

    Import TodoList into App.js, test working by displaying hello world to browser.

--- learn-react-in-x-minutes/src/App.js
+++ learn-react-in-x-minutes/src/App.js
@@ -1,7 +1,10 @@
-
+import TodoList from "./TodoList";
 
 function App() {
-  return null
+  return (
+    // create some html for the todo list using JSX
+    <TodoList />
+  )
 }
 
 export default App;
